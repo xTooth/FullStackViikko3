b33945d81c8acea0e26168e5af21cf04710f4db4
Kim Jämsen <jamsen.kim@gmail.com>
2019-02-08 12:31:00 +0200

    Heroku
    Heroku bugfixing

---
+++ build/build/precache-manifest.daccefb17fbaa43d9be61e3660ea4614.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d53f12b45f7821b2de97",
+    "url": "/static/js/main.d53f12b4.chunk.js"
+  },
+  {
+    "revision": "45a33658116e915bff91",
+    "url": "/static/js/1.45a33658.chunk.js"
+  },
+  {
+    "revision": "d53f12b45f7821b2de97",
+    "url": "/static/css/main.94eafa3f.chunk.css"
+  },
+  {
+    "revision": "3989779bc9123464dbe044eee76777fe",
+    "url": "/index.html"
+  }
+];
